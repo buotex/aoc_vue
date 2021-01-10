038513b866efddcd54db2dff2bef3ad04fb7cd09
Buote Xu <mail@buotexu.de>
2021-01-10 22:31:08 +0100

    added missing file

---
+++ src/Line.vue
@@ -0,0 +1,19 @@
+<script>
+import { Line, mixins } from "vue-chartjs";
+const { reactiveProp } = mixins;
+
+export default {
+  extends: Line,
+  mixins: [reactiveProp],
+  props: ["options"],
+  type: ["line"],
+  watch: {
+    chartData() {
+      this.renderChart(this.chartData, this.options);
+    },
+    options() {
+      this.renderChart(this.chartData, this.options);
+    }
+  }
+};
+</script>
